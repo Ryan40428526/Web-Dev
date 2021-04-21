a630f7591a702877c8f5a4b555593e713c339b9e
Ryan40428526 <82948394+Ryan40428526@users.noreply.github.com>
2021-04-21 13:20:35 +0100

    Website upload

---
+++ js script.js
@@ -0,0 +1,15 @@
+function WindowMessage(){
+	document.getElementById("WinOutput").innerHTML = "<p>You look through the window but can hardly make out anything due to how dark it is</p>"
+}
+
+function LookMessage(){
+	document.getElementById("LookOutput").innerHTML = "<p>You look around peering through broken doorways and under the rubble to find something, anything that would prove something was here</p>"
+}
+
+function SearchMessage(){
+	document.getElementById("SearchOutput").innerHTML = "<p>As you stumble around the room trying to find a light switch you realise the only light source is coming from above the end of the basement</p>"
+}
+
+function StareMessage(){
+	document.getElementById("StareOutput").innerHTML = "<p>As you stare at the creature you see it lift its blade like arm ready to kill you without hesitation</p>"
+}
